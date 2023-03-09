90ef1361bd0d1eebd8e732f0a62ea74b736328e6
Pedro Sena <phasena@gmail.com>
2023-03-09 20:56:42 -0300

    Novos exercícios e pequenas alterações
    Novos exercícios:
    Uma classe para triângulos
    Tipos de triângulos
    Alterações:
    Adicionado o enunciado dos exercicios

--- Exercicio_TamanhoDaMatriz.py
+++ Exercicio_TamanhoDaMatriz.py
@@ -1,3 +1,5 @@
+"""Escreva uma função dimensoes(matriz) que recebe uma matriz como parâmetro e imprime as dimensões da matriz recebida, no formato iXj."""
+
 def dimensoes(matriz):
     linha = len(matriz)
     coluna = len(matriz[0])
